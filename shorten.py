fc0f055899e679eedc4a044a40b3d099192a2a2e
Macailh <salvadorenriquegb@gmail.com>
2023-07-29 13:19:03 -0600

    feat: add shorten link method

--- shorten.py
+++ shorten.py
@@ -9,3 +9,30 @@ def generate_code():
     random_string = "".join(secrets.choice(alphanumeric) for i in range(9))
 
     return random_string
+
+
+def shorten_link(link):
+    code = generate_code()
+
+    data = []
+
+    with open("codes.json", "r") as file:
+        data = json.load(file)
+
+    exists = any(item["code"] == code for item in data)
+
+    if exists:
+        code = generate_code()
+
+    with open("codes.json", "w") as file:
+        data.append(
+            {
+                "code": code,
+                "website": f"http://localhost:5000/{code}",
+                "redirection": link
+            }
+        )
+        json.dump(data, file, indent=2)
+
+
+shorten_link("https://www.google.com")
