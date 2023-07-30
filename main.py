a8c4ec74a8480c6b2971b3f3f50eb36911468b77
Macailh <salvadorenriquegb@gmail.com>
2023-07-30 08:43:45 -0600

    feat: add endpoint for redirect urls

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse as redirect
+import json
+
+app = FastAPI()
+
+
+@app.get("/{code}")
+def redirection(code: str):
+    data = []
+
+    with open("codes.json", "r") as file:
+        data = json.load(file)
+
+    res = list(filter(lambda item: item["code"] == code, data))
+
+    if res:
+        return redirect(url=res[0]["redirection"], status_code=302)
+    return {"message": "Not found"}
