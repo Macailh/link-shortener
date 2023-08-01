cbb2fcc775030838bcf538fc26b1818775594291
Macailh <salvadorenriquegb@gmail.com>
2023-07-31 19:46:29 -0600

    docs: add methods docstring

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ app = FastAPI()
 
 @app.get("/{code}")
 def redirection(code: str):
+    """
+    Redirects the user to a specified URL based on the given code.
+
+    Parameters:
+    - code (str): A string representing the code used to look up the redirection URL.
+
+    Returns:
+    - RedirectResponse: If the provided code matches an entry in the 'codes.json' file,
+      a redirect response will be returned with a status code 302, redirecting the user
+      to the associated URL.
+    - dict: If the provided code is not found in the 'codes.json' file, a JSON object
+      will be returned with a "message" key set to "Not found".
+    """
     data = []
 
     with open("codes.json", "r", encoding="utf-8") as file:
--- shorten.py
+++ shorten.py
@@ -4,14 +4,29 @@ import string
 
 
 def generate_code():
+    """
+    Generates a random alphanumeric code of length 9.
+
+    Returns:
+    - str: A random alphanumeric code.
+    """
     alphanumeric = string.ascii_letters + string.digits
-
     random_string = "".join(secrets.choice(alphanumeric) for i in range(9))
-
     return random_string
 
 
 def shorten_link(link):
+    """
+    Shortens a given URL link by generating a unique code and saving the mapping
+    between the code and the original link in a JSON file.
+
+    Parameters:
+    - link (str): The URL link to be shortened.
+
+    Notes:
+    - This function will write the mapping between the generated code and the original link
+      to a JSON file named 'codes.json' in the current working directory.
+    """
     code = generate_code()
 
     data = []
